419e9732f33a047af07f39cdeee8cd4aff111ff4
atsuiiw <panwgo@gmail.com>
2026-02-28 20:03:34 +0700

    crud

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 
 // import routes
 import userRouter from '../routes/user.route.js'
+import postRouter from '../routes/post.route.js'
 
 // declare routes
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/post", postRouter);
 
 // example route: http://localhost:4000/api/v1/users/register
 
